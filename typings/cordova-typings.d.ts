97c3599a8f8c62f4b468e287030d84795c3213a1
Arathi22 <arathirajagopalan22@gmail.com>
2024-01-29 15:46:38 +0530

    bb

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="..\.vscode\typings\cordova\plugins\NetworkInformation.d.ts"/>
 /// <reference path="..\.vscode\typings\cordova\plugins\FileTransfer.d.ts"/>
 /// <reference path="..\.vscode\typings\cordova\plugins\Toast.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
